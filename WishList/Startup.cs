4620bd212dae48baf7a03be5a7d1a6769ed21ae6
damiancox78 <78485354+damiancox78@users.noreply.github.com>
2021-02-03 13:38:36 +0000

    Revert "Added Middleware/Configuration to startup"
    This reverts commit 68985998dc7dc0874aaf3040ba1febaf4c17e85e.

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -17,30 +17,15 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-        }   
-        
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.Run(async (context) =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                await context.Response.WriteAsync("Hello World!");
             });
-
         }
     }
 }
